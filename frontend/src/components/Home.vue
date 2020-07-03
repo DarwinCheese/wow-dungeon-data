37987d8aff7fa9bdaadc4cc3f40c7c6ecc18307a
Darwin Gutierrez <darwincheese@gmail.com>
2020-07-03 03:39:46 +0200

    First commit - RNG

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,35 @@
+<!-- Home.vue -->
+<template>
+  <div>
+    <p>Home page</p>
+    <p>Random number from backend: {{ randomNumber }}</p>
+    <button @click="getRandom">New random number</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      randomNumber: 0
+    }
+  },
+  methods: {
+    getRandomFromBackend () {
+      const path = `http://localhost:5000/api/random`
+      axios.get(path).then(response => {
+        this.randomNumber = response.data.randomNumber
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    getRandom () {
+      this.randomNumber = this.getRandomFromBackend()
+    }
+  },
+  created () {
+    this.getRandom()
+  }
+}
+</script>
